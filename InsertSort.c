2d901b7c97be7efb2b8dc3119b64ba936a14cac3
lengmuzashi <2690497440@qq.com>
2023-06-01 08:03:57 +0800

    直接插入排序

---
+++ InsertSort.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"Sort.h"
+//void PrintArray(int* a, int n)
+//{
+//	for (int i = 0; i < n; i++)
+//	{
+//		printf("%d ", a[i]);
+//	}
+//	printf("\n");
+//}
+void InsertSort(int* a, int n)//Ö±½Ó²åÈëÅÅÐò
+{
+	for (int i = 1; i < n; ++i)
+	{
+		int end = i - 1;
+		int tmp = a[i];
+
+		while (end >= 0)
+		{
+			if (a[end] < tmp)
+			{
+				a[end + 1] = a[end];
+				--end;
+			}
+			else
+			{
+				break;
+			}
+		}
+		a[end + 1] = tmp;
+	}
+}
